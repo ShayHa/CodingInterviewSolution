58d4761a0e0eeebbca303c3aa51a06fd303e1ab4
Shay Halfon <shayh@biyond.co.il>
2021-05-02 08:14:52 +0300

    Delete unneeded problems

--- sql/FriendRequests1.sql
+++
@@ -1,6 +0,0 @@
-SELECT ROUND(IFNULL(
-    (SELECT COUNT(DISTINCT requester_id, accepter_id) FROM request_accepted AS tmp1)
-    /
-    (SELECT COUNT(DISTINCT sender_id, send_to_id) FROM friend_request AS tmp2),0),2) AS accept_rate
-
-
